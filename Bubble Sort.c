b21f44ad0fcca9c7e81e0f19fa6e01b92dbda6a2
Annie1702 <76551841+Annie1702@users.noreply.github.com>
2021-01-30 19:11:49 +0530

    Add files via upload

---
+++ Bubble Sort.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void bsort(int[],int);
+void main()
+{
+    int i,j,m;
+    printf("Enter the no. of elements - ");
+    scanf("%d",&m);
+    int ar[m];
+    printf("Enter the elements - ");
+    for(i=0;i<m;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    bsort(ar,m);
+    printf("The Sorted Array is - \n");
+    for(i=0;i<m;i++)
+    {
+        printf("%d ",ar[i]);
+    }
+}
+void bsort(int ar[],int m)
+{
+    int temp,i,j,a=0;
+    for(i=0;i<(m-1);i++)
+    {
+        for(j=0;j<(m-1);j++)
+        {
+            if(ar[j]>ar[j+1])
+            {
+                temp = ar[j];
+                ar[j] = ar[j+1];
+                ar[j+1] = temp;
+            }
+        }
+        printf("Array in Pass %d - ",++a);
+        for(int k=0;k<m;k++)
+        {
+            printf("%d ",ar[k]);
+        }
+        printf("\n");
+    }
+}
