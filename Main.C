c41303eff52f39428fff0882b91f5bb9cef56a09
Annie1702 <76551841+Annie1702@users.noreply.github.com>
2021-01-30 19:59:02 +0530

    Update Main.C

--- Main.C
+++ Main.C
@@ -12,10 +12,10 @@ void main()
   void quad();
   void taylor();
   void binomial();
+  void bsort();
   int ch,a;
-  clrscr();
   loop:
-  printf("\n1. Calculator Program\n2. Palindrome Program\n3. Root Program\n4. Taylor Series Program\n5. Binomial Function\n6. Exit\nWhich program you wanna continue with - ");
+  printf("\n1. Calculator Program\n2. Palindrome Program\n3. Root Program\n4. Taylor Series Program\n5. Binomial Function\n6. Binary Sorting\n7. Exit\nWhich program you wanna continue with - ");
   scanf("%d",&ch);
   switch(ch)
   {
@@ -40,51 +40,57 @@ void main()
       break;
 
     case 6:
+      bsort();
+      break;
+
+    case 7:
       break;
 
     default:
-      printf("Enter valid option\n");
+      printf("\nEnter valid option and try again\n");
+      goto loop;
   }
-  if(ch!=6)
-  { 
-      printf("Do you wanna continue (1 -> Y / 0 -> N) - "); 
+  if(ch!=7)
+  {
+      printf("\n\nDo you wanna continue (1 -> Yes / 0 -> No) - ");
       scanf("%d",&a);
   }
   if(a==1)
-  { goto loop; }
-  getch();
+  { 
+      goto loop; 
+  }
 }
 
 void calc()
 {
-	int a,b,s=0,choice;
+	int a,b,s=0,ch;
     printf("\nEnter first number -  ");
     scanf("%d",&a);
     printf("Enter second number -  ");
     scanf("%d",&b);
-    printf("\n1. Addition\n2. Subtration\n3. Multiplication\n4. Division\n5. Modulus\nEnter your choice - ");
-    scanf("%d",&choice);
-    switch(choice)
+    printf("\n1. Addition\n2. Subtration\n3. Multiplication\n4. Division\n5. Modulus\n\nEnter your choice - ");
+    scanf("%d",&ch);
+    switch(ch)
     {
     case 1:
     	s=a+b;
-	printf("\nAddition of %d & %d is %d\n",a,b,s);
+	printf("\nAddition of %d & %d is %d",a,b,s);
         break;
         
     case 2:
         s=a-b;
-	printf("\nSubtraction of %d & %d is %d\n",a,b,s);
+	printf("\nSubtraction of %d & %d is %d",a,b,s);
 	break;
 
     case 3:
 	s=a*b;
-	printf("\nMultiplication of %d & %d is %d\n",a,b,s);
+	printf("\nMultiplication of %d & %d is %d",a,b,s);
 	break;
 
     case 4:
 	if(b==0)
 	{
-	    printf("\nDivision not possible as number 2 is \n0");
+	    printf("\nDivision not possible as denominator is 0");
 	    break;
 	}
 	else
@@ -97,25 +103,26 @@ void calc()
     case 5:
 	if(b==0)
 	{
-	       printf("\nModulus not possible as number 2 is 0\n");
+	       printf("\nModulus not possible as denominator is 0");
 	    }
 	    else
 	    {
 	       s=a%b;
-	       printf("\nModulus of %d & %d is %d\n",a,b,s);
+	       printf("\nModulus of %d & %d is %d",a,b,s);
 	    }
 	    break;
 
     default:
-	printf("Enter valid options\n");
+	printf("Enter valid options");
     }
 }
 
 void palin()
 {
-    int num,rev=0,i,a,temp;
+    long int a;
+    int num,rev=0,i,temp;
     printf("\nEnter the number -  ");
-    scanf("%d",&a);
+    scanf("%ld",&a);
     num=a;
     for(;a!=0;)
     {
@@ -125,11 +132,11 @@ void palin()
     }
     if(rev==num)
     {
-	printf("\nIt is a palindrome\n");
+	printf("\nIt is a palindrome");
     }
     else
     {
-	printf("\nIt is not a palindrome\n");
+	printf("\nIt is not a palindrome");
     }
 }
 
@@ -142,22 +149,22 @@ void quad()
     if(d==0)
     {
         x1 = x2 = -b/(2*a);
-        printf("\nRoots are real and equal\n");
-        printf("The roots are \n x1 = x2 = %f\n",x1);
+        printf("\nRoots are real and equal");
+        printf("\nThe roots are \nx1 = x2 = %f",x1);
     }
     else if(d>0)
     {
         x1 = (-b + sqrt(d))/(2*a);
         x2 = (-b - sqrt(d))/(2*a);
-        printf("\nRoots are real and distinct\n");
-        printf("The roots are \n x1 = %f\n x2 = %f\n",x1,x2);
+        printf("\nRoots are real and distinct");
+        printf("\nThe roots are \nx1 = %f\nx2 = %f",x1,x2);
     }
     else
     {
         x1 = -b/(2*a);
         x2 = sqrt(fabs(d))/(2*a);
-        printf("\nRoots are imaginary\n");
-	printf("The roots are \n x1 = %f + i %f \nx2 = %f - i %f\n",x1,x2,x1,x2);
+        printf("\nRoots are imaginary");
+	printf("\nThe roots are \nx1 = %f + i %f \nx2 = %f - i %f",x1,x2,x1,x2);
     }
 }
 
@@ -180,8 +187,8 @@ void taylor()
         i = i+2;
     }
     while(fabs(term)>= FLT_EPSILON);
-    printf("\nThe sin(%d) = %.3f",degree,sum);
-    printf("Using library function, the sin(%d) = %.3f\n",degree,sin(x));
+    printf("\nThe sin(%d) = %.3f\n",degree,sum);
+    printf("Using library function, the sin(%d) = %.3f",degree,sin(x));
 }
 
 void binomial()
@@ -190,7 +197,7 @@ void binomial()
     printf("Enter the value of n and r - ");
     scanf("%d %d",&n,&r);
     nCr = fact(n)/(fact(r)*fact(n-r));
-    printf("Value of nCr is %d\n",nCr);
+    printf("Value of nCr is %d",nCr);
 }
 
 int fact(int n)
@@ -202,5 +209,42 @@ int fact(int n)
     else
     {
         return (n*fact(n-1));
+    }
+}
+
+
+void bsort()
+{
+    int i,j,m,temp,a=0;
+    printf("Enter the no. of elements - ");
+    scanf("%d",&m);
+    int ar[m];
+    printf("\nEnter the elements - ");
+    for(i=0;i<m;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<(m-1);j++)
+        {
+            if(ar[j]>ar[j+1])
+            {
+                temp = ar[j];
+                ar[j] = ar[j+1];
+                ar[j+1] = temp;
+            }
+        }
+        printf("Array in Pass %d - ",++a);
+        for(int k=0;k<m;k++)
+        {
+            printf("%d ",ar[k]);
+        }
+        printf("\n");
+    }
+    printf("The Sorted Array is - \n");
+    for(i=0;i<m;i++)
+    {
+        printf("%d ",ar[i]);
     }
 }
