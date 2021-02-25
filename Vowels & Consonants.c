af9ece06260698d44e44d50434c3fea5a69fafd5
Annie1702 <76551841+Annie1702@users.noreply.github.com>
2021-02-25 22:50:09 +0530

    Update Vowels & Consonants.c

--- Vowels & Consonants.c
+++ Vowels & Consonants.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
+#include<conio.h>
 #include<string.h>
 #include<ctype.h>
 void main()
 {
     char ar[50];
     int j,v,c=0,a=0,e=0,i=0,o=0,u=0,n=0;
+    clrscr();
     printf("Enter the string - ");
     scanf("%s",ar);
     for(j=0;j<strlen(ar);j++)
@@ -42,4 +44,5 @@ void main()
         }
     }
     printf("Vowels = %d\nNo.of A/a = %d\nNo.of E/e = %d\nNo.of I/i = %d\nNo.of O/o = %d\nNo.of U/u = %d\nConsonants = %d\nNumbers = %d\n",a+e+i+o+u,a,e,i,o,u,c,n);
+    getch();
 }
